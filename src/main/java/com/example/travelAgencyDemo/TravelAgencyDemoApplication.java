519a79a498e34876f8319d3df9a73ed459b9f41f
Zofia Styszynska <zofia.styszynska@gmail.com>
2020-12-19 22:57:23 +0100

    first commit

---
+++ src/main/java/com/example/travelAgencyDemo/TravelAgencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelAgencyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyDemoApplication.class, args);
+	}
+
+}
